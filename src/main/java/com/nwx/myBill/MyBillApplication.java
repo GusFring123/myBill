99cedc92e1471c08a1901cf084383699ede94d5d
utra <1433372073@qq.com>
2018-03-26 11:04:53 +0800

    initial commit

---
+++ src/main/java/com/nwx/myBill/MyBillApplication.java
@@ -0,0 +1,12 @@
+package com.nwx.myBill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBillApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nwx/myBill/MyBillApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nwx.myBill;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyBillApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
